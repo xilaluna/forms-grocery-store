2ccfb7a609817d57388b0a7d9decdb9dcafdf64d
Xila Luna <xilaluna2@gmail.com>
2021-02-09 17:12:53 -0800

    forms work

--- grocery_app/forms.py
+++ grocery_app/forms.py
@@ -1,21 +1,28 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, DateField, SelectField, SubmitField
+from wtforms import StringField, DateField, SelectField, SubmitField, FloatField
 from wtforms.ext.sqlalchemy.fields import QuerySelectField
-from wtforms.validators import DataRequired, Length, URL
+from wtforms.validators import DataRequired, Length, MacAddress, URL
+from grocery_app import ItemCategory
 
 
 class GroceryStoreForm(FlaskForm):
     """Form for adding/updating a GroceryStore."""
+    title = StringField('Store Name', validators=[DataRequired()])
+    address = StringField('Address')
+    submit = SubmitField('Submit')
 
     # TODO: Add the following fields to the form class:
     # - title - StringField
     # - address - StringField
     # - submit button
-    pass
 
 
 class GroceryItemForm(FlaskForm):
     """Form for adding/updating a GroceryItem."""
+
+    name = StringField('Item Name')
+    price = FloatField('Price')
+    category = SelectField('Category', choices=ItemCategory.choices())
 
     # TODO: Add the following fields to the form class:
     # - name - StringField
